d706ca1e6b1578db719899f11c864f2cd2121402
Shayan Hosseinzadeh <shayanhosseinzadeh1@gmail.com>
2024-05-25 11:56:57 +0330

    Configed Admin panel

--- tickets/admin.py
+++ tickets/admin.py
@@ -6,10 +6,14 @@ from tickets.models import Ticket, Message
 class MessageInline(admin.TabularInline):
     model = Message
     extra = 1
+
+
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'user', 'assigned_to', 'created_at', 'status']
-    search_fields = ['id', 'title', 'user']
+    search_fields = ['id', 'title']
+    list_display_links = ['title']
+    list_editable = ['status']
     inlines = [MessageInline]
 
 
